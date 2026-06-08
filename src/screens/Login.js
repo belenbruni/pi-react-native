9a337baf5be6985181b28474a369ce5da967e334
biancapazudesa <pazb@udesa.edu.ar>
2026-06-08 15:10:21 -0300

    Hice la screen de log in, cambie el register para que tenga las validaciones. Ademas hice los estilos de ambas screens

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,9 +1,49 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable, TextInput } from 'react-native';
+import NavegationTab from "../navegation/NavegationTab"
+import { useState } from "react"
+import { auth } from "../firebase/config"
 
-export default function Login(){
-    return(
+export default function Login(props) {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [login, setLogin] = useState(false)
+    const [loginError, setLoginError] = useState("")
+
+    function onSubmit() {
+        console.log(email)
+        console.log(password)
+        auth.signInWithEmailAndPassword(email, password)
+            .then(res => {
+                setLogin(true)
+                props.navigation.navigate("NavegationTab")
+            })
+            .catch(error => {
+                console.log(error);
+                setLoginError("Credenciales inválidas")
+            })
+    }
+
+    return (
         <View style={styles.container}>
-            <Text style={styles.text}>Login</Text>
+            <Text style={styles.title}>Login</Text>
+            <TextInput style={styles.campos}
+                keyboardType="email-address"
+                placeholder="Email"
+                onChangeText={text => setEmail(text)}
+                value={email} />
+            <TextInput style={styles.campos}
+                keyboardType="email-address"
+                placeholder="Contraseña"
+                secureTextEntry={true}
+                onChangeText={text => setPassword(text)}
+                value={password} />
+            <Pressable style={styles.button} onPress={onSubmit}>
+                <Text style={styles.buttonText}> Entrar en la app </Text>
+            </Pressable>
+            <Text>{loginError}</Text>
+            <Pressable style={styles.button} onPress={() => props.navigation.navigate("Register")}>
+                <Text style={styles.buttonText}>Ir al registro</Text>
+            </Pressable>
         </View>
     )
 }
@@ -13,11 +53,43 @@ const styles = StyleSheet.create({
     container:{
         flex:1,
         justifyContent:'center',
-        alignItems:'center'
+        alignItems:'center',
+        paddingHorizontal: 10,
+        marginTop: 20
     },
 
-    text:{
-        fontSize:30
-    }
+    title:{
+        fontSize:32,
+        fontWeight:'bold',
+        marginBottom: 10,
+    },
+
+    button:{
+        backgroundColor: "#ed58a4",
+        paddingVertical: 10,
+        paddingHorizontal:20,
+        borderRadius:10,
+        width:200,
+        alignItems:'center',
+        margin: 5
+    },
+
+    buttonText:{
+        color:'white',
+        fontSize:16
+    },
+    campos:{
+        color: "#ed58a4", 
+        width: 250,
+        borderColor: "#ed58a4",
+        borderWidth: 1,
+        borderRadius: 10, 
+        paddingHorizontal: 10, 
+        backgroundColor: "white",
+        height: 20, 
+        paddingVertical: 15,
+        margin: 8
+    },
+
 
 })
--- src/screens/Register.js
+++ src/screens/Register.js
@@ -13,7 +13,14 @@ export default function Register(props) {
         console.log(email)
         console.log(username)
         console.log(password)
-
+        if (email.includes("@") == false) {
+            setRegisterError("Email mal formateado")
+            return
+        }
+        if (password.length < 6) {
+            setRegisterError("La password debe tener la longitud minima de 6 caracteres")
+            return
+        }
         auth.createUserWithEmailAndPassword(email, password)
             .then(res => {
                 db.collection('users').add({
@@ -22,12 +29,13 @@ export default function Register(props) {
                     createdAt: Date.now()
                 })
                 setRegister(true);
+                props.navigation.navigate("Login");
             })
             .catch(error => {
-                console.log(error);
-                setRegisterError('Fallo en el registro')
+                console.log('error de firebase', error);
+                setRegisterError(error.message)
+                return
             })
-            props.navigation.navigate("Login");
     }
 
     return (
@@ -49,6 +57,7 @@ export default function Register(props) {
                 secureTextEntry={true}
                 onChangeText={text => setPassword(text)}
                 value={password} />
+            <Text>{registerError}</Text>
             <Pressable style={styles.button} onPress={() => onSubmit()}>
                 <Text style={styles.buttonText}> Registrarme </Text>
             </Pressable>
@@ -64,27 +73,41 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
+        paddingHorizontal: 10,
+        marginTop: 20
+    },
+
+    title: {
+        fontSize: 32,
+        fontWeight: 'bold'
+    },
+
+    button: {
+        backgroundColor: "#ed58a4",
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        borderRadius: 10,
+        width: 200,
+        alignItems: 'center',
+        margin: 5
+    },
+
+    buttonText: {
+        color: 'white',
+        fontSize: 16
     },
     campos: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
+        color: "#ed58a4",
+        width: 250,
+        borderColor: "#ed58a4",
+        borderWidth: 1,
+        borderRadius: 10,
+        paddingHorizontal: 10,
+        backgroundColor: "white",
+        height: 20,
+        paddingVertical: 15,
+        margin: 8
     },
-    title: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    button: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    buttonText: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
 
 })
